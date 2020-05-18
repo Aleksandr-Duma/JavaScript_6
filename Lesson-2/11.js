d57e6982d895fe4d2b921a1a32cf09511916ce20
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-05-18 23:45:08 +0300

    Finish commit

--- Lesson-2/11.js
+++ Lesson-2/11.js
@@ -6,3 +6,12 @@
 // const array = [2, -1, -3, 15, 0, 4];
 
 ////////////////// Решение //////////////////
+const array = [2, -1, -3, 15, 0, 4];
+let result = null;
+for(let i = 0; i < array.length; i++){
+    if(array[i] <= 0){
+        continue;
+    }
+    result += array[i];
+}
+console.log(result);
