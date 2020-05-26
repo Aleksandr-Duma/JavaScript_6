7767808f9d12e63f09915630019a2685bc2d8ea6
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-05-26 20:57:02 +0300

    First example commit

---
+++ Lesson-3/1.js
@@ -0,0 +1,28 @@
+/**
+ * Задача 1.
+ *
+ * Напишите функцию upperCaseFirst(string).
+ * Функция преобразовывает первый символ переданной строки в заглавный и возвращает новую строку.
+ *
+ * Условия:
+ * - Функция принимает один параметр;
+ * - Функция содержит валидацию входного параметра на тип string.
+ */
+
+// Решение
+
+function upperCaseFirst(param){
+    if(typeof param !== 'string'){
+        console.log('Error!!!');
+    }
+    let firstLetter = param.charAt(0).toUpperCase();
+    let string = param.substr(1).toLowerCase();
+    let result = firstLetter + string;
+
+        console.log(result);
+}
+
+// upperCaseFirst('pitter'); // Pitter
+upperCaseFirst(' '); // ''
+
+// exports.upperCaseFirst = upperCaseFirst;
