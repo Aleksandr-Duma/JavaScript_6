6628129e4114de4950eb3e36d5ec31294e902a00
Aleksandr_Duma <Dumaweb9@gmail.com>
2020-06-18 12:50:56 +0300

    Second task done commit

--- Lesson-7/2.js
+++ Lesson-7/2.js
@@ -20,19 +20,61 @@
 
 // Решение
 
-const array1 = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
-console.log(collect(array1)); // 12
+const isArrayArgument = function(arg){
+    if(!Array.isArray(arg)){
+        throw new Error('Вы передали не массив!')
+    }
+};
 
-const array2 = [[[[[1, 2]]]]];
-console.log(collect(array2)); // 3
+const flatArray = function(arr){
+    let array = [];
 
-const array3 = [[[[[1, 2]]], 2], 1];
-console.log(collect(array3)); // 6
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+          array = array.concat(flatArray(arr[i])); 
+        }else{
+            array.push(arr[i]);
+        }
+    }
 
-const array4 = [[[[[]]]]];
-console.log(collect(array4)); // 0
+    return array;
+};
 
-const array5 = [[[[[], 3]]]];
-console.log(collect(array5)); // 3
+const collect = function(arr){
+    
+    let result = 0;
+    let arrayFlatten = flatArray(arr);
+    
+    isArrayArgument(arr);
+
+    result = arrayFlatten.reduce(function(accum, elem){
+        if(!Array.isArray(elem) && typeof elem !== 'number'){
+            throw new Error('Елемент не является ни числом, ни массивом!');
+        }
+        return accum + elem;
+    }, 0);
+
+    return result;
+}
+
+try{
+    const array1 = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+    console.log(collect(array1)); // 12
+
+    const array2 = [[[[[1, 2]]]]];
+    console.log(collect(array2)); // 3
+
+    const array3 = [[[[[1, 2]]], 2], 1];
+    console.log(collect(array3)); // 6
+
+    const array4 = [[[[[]]]]];
+    console.log(collect(array4)); // 0
+
+    const array5 = [[[[[], 3]]]];
+    console.log(collect(array5)); // 3
+
+}catch(err){
+    console.log(err.message);
+}
 
 exports.collect = collect;
